793e3b332803c99cabd76b0a1901baa1cea103ea
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-03-23 16:03:27 -0500

    basic styling of filter search bar

--- frontend/Components/SearchBar/index.js
+++ frontend/Components/SearchBar/index.js
@@ -2,31 +2,32 @@ import React, { Component } from "react";
 
 import styles from "./styles";
 
-import Button from "Components/Button";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { search } from "../../Views/ForumFeed/actions";
 
 class SearchBar extends Component {
   render() {
-      const { search, searchInput } = this.props;
+    const { search, searchInput } = this.props;
 
-      return (
-          <div>
-              <label for="search"> Search Discussions: 
-      <input className={styles.search} type="text" placeholder="Search..."
-                      onChange={(e) => search(e.target.value)}
-                      value={searchInput}
-                  />
-    </label>
-             
-        
+    return (
+      <div className={styles.search}>
+        <label className={styles.searchLabel} for="search">
+          Search Discussions:
+        </label>
+        <input
+          className={styles.searchBox}
+          type="text"
+          placeholder="Search..."
+          onChange={e => search(e.target.value)}
+          value={searchInput}
+        />
       </div>
     );
   }
 }
-function mapStateToProps({ searchInput }) { 
- return {searchInput}
+function mapStateToProps({ searchInput }) {
+  return { searchInput };
 }
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ search }, dispatch);
