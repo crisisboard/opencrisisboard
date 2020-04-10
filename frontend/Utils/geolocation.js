60254116a84d1f9a4c8327ba9b1ade77ab1318de
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-04-10 12:23:29 -0300

    Method to search address by lat and lng. #38

--- frontend/Utils/geolocation.js
+++ frontend/Utils/geolocation.js
@@ -1,14 +1,20 @@
-import { OpenStreetMapProvider } from 'leaflet-geosearch';
+import { OpenStreetMapProvider } from "leaflet-geosearch";
 
-const MAP_DEFAULT_CENTER_LATITUDE = process.env.REACT_APP_MAP_DEFAULT_CENTER_LATITUDE;
-const MAP_DEFAULT_CENTER_LONGITUDE = process.env.REACT_APP_MAP_DEFAULT_CENTER_LATITUDE;
+const MAP_DEFAULT_CENTER_LATITUDE =
+  process.env.REACT_APP_MAP_DEFAULT_CENTER_LATITUDE;
+const MAP_DEFAULT_CENTER_LONGITUDE =
+  process.env.REACT_APP_MAP_DEFAULT_CENTER_LONGITUDE;
 
 export const getDefaultCenter = () => {
-  console.log('default center:', MAP_DEFAULT_CENTER_LATITUDE, MAP_DEFAULT_CENTER_LONGITUDE);
+  console.log(
+    "default center:",
+    MAP_DEFAULT_CENTER_LATITUDE,
+    MAP_DEFAULT_CENTER_LONGITUDE
+  );
   return {
-    lat: MAP_DEFAULT_CENTER_LATITUDE,
-    lng: MAP_DEFAULT_CENTER_LONGITUDE
-  }
+    lat: parseFloat(MAP_DEFAULT_CENTER_LATITUDE),
+    lng: parseFloat(MAP_DEFAULT_CENTER_LONGITUDE),
+  };
 };
 
 /**
@@ -18,12 +24,12 @@ export const getDefaultCenter = () => {
  */
 export const getBrowserLocation = (successCallback, errorCallback) => {
   if (!navigator.geolocation) {
-    errorCallback('User has denied access to the location in their browser');
+    errorCallback("User has denied access to the location in their browser");
   } else {
-    navigator.geolocation.getCurrentPosition(geoLocation => {
+    navigator.geolocation.getCurrentPosition((geoLocation) => {
       successCallback({
         lat: geoLocation.coords.latitude,
-        lng: geoLocation.coords.longitude
+        lng: geoLocation.coords.longitude,
       });
     }, errorCallback);
   }
@@ -38,27 +44,63 @@ export const getGeolocationFromAddress = async (address) => {
   const geoLocation = {};
 
   return await new OpenStreetMapProvider()
-  .search({ query: address })
-  .then(
-    response => {
-      console.log( response);
-      if (response && response.length > 0) {
-        geoLocation.lat = parseFloat(response[0].y);
-        geoLocation.lng = parseFloat(response[0].x);
-      } else {
-        // This case should only be hit if leaflet-geosearch breaks
-        geoLocation.error = 'Response from leaflet-geosearch was incorrectly formed'
+    .search({ query: address })
+    .then(
+      (response) => {
+        console.log(response);
+        if (response && response.length > 0) {
+          geoLocation.lat = parseFloat(response[0].y);
+          geoLocation.lng = parseFloat(response[0].x);
+        } else {
+          // This case should only be hit if leaflet-geosearch breaks
+          geoLocation.error =
+            "Response from leaflet-geosearch was incorrectly formed";
+        }
+        return geoLocation;
+      },
+      (error) => {
+        geoLocation.error = error;
+        return geoLocation;
       }
-      return geoLocation;
-    },
-    error => {
+    )
+    .catch((error) => {
       geoLocation.error = error;
       return geoLocation;
-    }
-  )
-  .catch(error => {
-    geoLocation.error = error;
-    return geoLocation;
-  });
+    });
 };
 
+/**
+ * Get address information from a latitude and logitude.
+ * @param {Number} lat
+ * @param {Number} lng
+ */
+export const getGeolocationFromLatLng = async (lat, lng) => {
+  const response = await new OpenStreetMapProvider({
+    params: {
+      lat,
+      lon: lng,
+    },
+  }).search({
+    data: {
+      raw: {
+        osm_id: 1,
+      },
+    },
+  });
+
+  const [
+    {
+      raw: { address },
+      label: label,
+    },
+  ] = response;
+
+  const position = {
+    lat,
+    lng,
+    label,
+    address,
+  };
+
+  return position;
+};
